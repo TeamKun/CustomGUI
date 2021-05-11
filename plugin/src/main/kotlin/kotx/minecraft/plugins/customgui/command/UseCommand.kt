5e9907d400b9bc1899e27184285f471fbe63a507
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-11 20:23:56 +0900

    🔧 Update: support new flylib

--- plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/command/UseCommand.kt
+++ plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/command/UseCommand.kt
@@ -3,7 +3,7 @@ package kotx.minecraft.plugins.customgui.command
 import kotx.ktools.asPacket
 import kotx.ktools.toJson
 import kotx.minecraft.libs.flylib.command.Command
-import kotx.minecraft.libs.flylib.command.CommandContext
+import kotx.minecraft.libs.flylib.command.internal.Argument
 import kotx.minecraft.libs.flylib.command.internal.Usage
 import kotx.minecraft.plugins.customgui.directory.Directories
 import java.nio.file.Paths
@@ -11,42 +11,31 @@ import java.nio.file.Paths
 class UseCommand : Command("use") {
     override val description: String = "指定したファイルをワークスペースに読み込みます。(現在のワークスペースは破棄されます。)"
     override val usages: List<Usage> = listOf(
-        Usage("use <targetFile>")
+        Usage(Argument.Text("target_file") { Directories.guis.files.map { it.nameWithoutExtension } }) {
+            val fileName = args.first().replace("[/\\\\.]".toRegex(), "")
+            val gui = Directories.guis.files.find { it.nameWithoutExtension == fileName }
+            if (gui == null) {
+                sendErrorMessage("${fileName}は存在しません。")
+                return@Usage
+            }
+
+            val config = Paths.get("plugins", "CustomGUI", "workspaces", "${player?.uniqueId}.json").toFile()
+            config.parentFile.mkdirs()
+            if (!config.exists())
+                config.createNewFile()
+
+            config.writeText(gui.readText())
+
+            println(gui.readText())
+            player?.sendPluginMessage(plugin, "customgui:workspace", object {
+                val op = 0
+                val data = gui.readText()
+            }.toJson().asPacket())
+
+            sendSuccessMessage("${fileName}を現在のワークスペースに読み込みました。")
+        }
     )
     override val examples: List<String> = listOf(
         "customgui use TestGUI"
     )
-
-
-    override fun CommandContext.execute() {
-        if (args.isEmpty()) {
-            sendHelp()
-            return
-        }
-
-        val fileName = args.first().replace("[/\\\\.]".toRegex(), "")
-        val gui = Directories.guis.files.find { it.nameWithoutExtension == fileName }
-        if (gui == null) {
-            sendErrorMessage("${fileName}は存在しません。")
-            return
-        }
-
-        val config = Paths.get("plugins", "CustomGUI", "workspaces", "${player?.uniqueId}.json").toFile()
-        config.parentFile.mkdirs()
-        if (!config.exists())
-            config.createNewFile()
-
-        config.writeText(gui.readText())
-
-        println(gui.readText())
-        player?.sendPluginMessage(plugin, "customgui:workspace", object {
-            val op = 0
-            val data = gui.readText()
-        }.toJson().asPacket())
-
-        sendSuccessMessage("${fileName}を現在のワークスペースに読み込みました。")
-    }
-
-    override fun CommandContext.tabComplete() =
-        if (args.size == 1) Directories.guis.files.map { it.nameWithoutExtension } else emptyList()
 }
