c1c341825e46c31aa63d26cfacc80d88c0c4d5f4
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-11 20:14:16 +0900

    🔧 Update: remove permission

--- plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/command/CustomGUICommand.kt
+++ plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/command/CustomGUICommand.kt
@@ -2,11 +2,9 @@ package kotx.minecraft.plugins.customgui.command
 
 import kotx.minecraft.libs.flylib.command.Command
 import kotx.minecraft.libs.flylib.command.CommandContext
-import kotx.minecraft.libs.flylib.command.internal.Permission
 
 class CustomGUICommand : Command("customgui") {
     override val description: String = "CustomGUIを制御するコマンド"
-    override val permission: Permission = Permission.EVERYONE
 
     override val children: List<Command> = listOf(
         AddCommand(),
