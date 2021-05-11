79a760503909d1c83167e6543e4770fa18369429
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-11 20:15:40 +0900

    🔧 Update: support new forge mapping

--- mod/src/main/kotlin/kotx/customgui/Extensions.kt
+++ mod/src/main/kotlin/kotx/customgui/Extensions.kt
@@ -14,22 +14,22 @@ fun FontRenderer.drawStringCentered(
     color: Color,
     shadow: Boolean = false
 ) {
-    val w = width(text)
-    val h = wordWrapHeight(text, Int.MAX_VALUE)
+    val w = getStringWidth(text)
+    val h = getWordWrappedHeight(text, Int.MAX_VALUE)
     val xPos = (x - w / 2)
     val yPos = (y - h / 2)
 
     if (shadow)
-        drawShadow(stack, text, xPos.toFloat(), yPos.toFloat(), color.rgb)
+        drawStringWithShadow(stack, text, xPos.toFloat(), yPos.toFloat(), color.rgb)
     else
-        draw(stack, text, xPos.toFloat(), yPos.toFloat(), color.rgb)
+        drawString(stack, text, xPos.toFloat(), yPos.toFloat(), color.rgb)
 }
 
 fun FontRenderer.drawString(stack: MatrixStack, text: String, x: Int, y: Int, color: Color, shadow: Boolean = false) {
     if (shadow)
-        drawShadow(stack, text, x.toFloat(), y.toFloat(), color.rgb)
+        drawStringWithShadow(stack, text, x.toFloat(), y.toFloat(), color.rgb)
     else
-        draw(stack, text, x.toFloat(), y.toFloat(), color.rgb)
+        drawString(stack, text, x.toFloat(), y.toFloat(), color.rgb)
 }
 
 fun fill(matrixStack: MatrixStack, x: Int, y: Int, width: Int, height: Int, color: Color) {
@@ -47,10 +47,10 @@ fun fillAbsolute(matrixStack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, c
 }
 
 val scaledWidth: Int
-    get() = Minecraft.getInstance().window.guiScaledWidth
+    get() = Minecraft.getInstance().mainWindow.scaledWidth
 
 val scaledHeight: Int
-    get() = Minecraft.getInstance().window.guiScaledHeight
+    get() = Minecraft.getInstance().mainWindow.scaledHeight
 
 val xCenter: Int
     get() = scaledWidth / 2
