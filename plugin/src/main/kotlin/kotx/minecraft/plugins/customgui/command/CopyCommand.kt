3f46798fa0f2c997d5998f73fde927f4d37a123c
Kotlin-Chan <developer.hazuku@gmail.com>
2021-05-11 20:13:21 +0900

    🔧 Update: Support new flylib, use usages instead of execute()

--- plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/command/CopyCommand.kt
+++ plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/command/CopyCommand.kt
@@ -1,8 +1,7 @@
 package kotx.minecraft.plugins.customgui.command
 
 import kotx.minecraft.libs.flylib.command.Command
-import kotx.minecraft.libs.flylib.command.CommandContext
-import kotx.minecraft.libs.flylib.command.internal.Permission
+import kotx.minecraft.libs.flylib.command.internal.Argument
 import kotx.minecraft.libs.flylib.command.internal.Usage
 import kotx.minecraft.plugins.customgui.directory.Directories
 
@@ -11,40 +10,30 @@ class CopyCommand : Command("copy") {
     override val description: String = "指定したファイルを自分の物としてコピーします。"
     override val usages: List<Usage> = listOf(
         Usage(
-            "copy <from> <to>"
-        )
+            Argument.Text("from") {
+                Directories.guis.files.map { it.nameWithoutExtension }
+            },
+            Argument.Text("to"),
+        ) {
+            val fromFileName = args.first().replace("[/\\\\.]".toRegex(), "")
+            val toFileName = args[1].replace("[/\\\\.]".toRegex(), "")
+            val fromFile = Directories.guis.files.find { fromFileName == it.nameWithoutExtension }
+
+            if (fromFile == null) {
+                sendErrorMessage("${toFileName}は見つかりませんでした。")
+                return@Usage
+            }
+
+            if (Directories.guis.files.any { toFileName == it.nameWithoutExtension }) {
+                sendErrorMessage("${toFileName}は既に存在しています。")
+                return@Usage
+            }
+
+            Directories.guis.write("${player!!.uniqueId}/$toFileName.json", fromFile.readText())
+            sendSuccessMessage("${fromFileName}を${toFileName}にコピーしました")
+        }
     )
     override val examples: List<String> = listOf(
         "customgui copy TestGUI CopiedTestGUI"
     )
-
-    override val permission: Permission = Permission.EVERYONE
-
-
-    override fun CommandContext.execute() {
-        if (args.size != 2) {
-            sendHelp()
-            return
-        }
-
-        val fromFileName = args.first().replace("[/\\\\.]".toRegex(), "")
-        val toFileName = args[1].replace("[/\\\\.]".toRegex(), "")
-        val fromFile = Directories.guis.files.find { fromFileName == it.nameWithoutExtension }
-
-        if (fromFile == null) {
-            sendErrorMessage("${toFileName}は見つかりませんでした。")
-            return
-        }
-
-        if (Directories.guis.files.any { toFileName == it.nameWithoutExtension }) {
-            sendErrorMessage("${toFileName}は既に存在しています。")
-            return
-        }
-
-        Directories.guis.write("${player!!.uniqueId}/$toFileName.json", fromFile.readText())
-        sendSuccessMessage("${fromFileName}を${toFileName}にコピーしました")
-    }
-
-    override fun CommandContext.tabComplete() =
-        if (args.size == 1) Directories.guis.files.map { it.nameWithoutExtension } else emptyList()
 }
