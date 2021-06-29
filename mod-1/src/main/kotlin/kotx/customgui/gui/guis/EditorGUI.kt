9284384bf2890627008433fe092bf625dc277a3d
kotx <developer.hazuku@gmail.com>
2021-06-30 04:29:31 +0900

    🚧 Wip!

--- mod-1/src/main/kotlin/kotx/customgui/gui/guis/EditorGUI.kt
+++ mod-1/src/main/kotlin/kotx/customgui/gui/guis/EditorGUI.kt
@@ -2,8 +2,10 @@ package kotx.customgui.gui.guis
 
 import com.mojang.blaze3d.matrix.*
 import kotx.customgui.gui.*
+import kotx.customgui.view.*
 
 object EditorGUI : GUI() {
+    val holders = mutableListOf<ViewHolder>()
     override fun draw(stack: MatrixStack, mouseX: Int, mouseY: Int) {
 
     }
