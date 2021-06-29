76208c76e0381169b19fefaa71d7a318e8d7ec7f
kotx <developer.hazuku@gmail.com>
2021-06-30 00:29:44 +0900

    New JsonBuilder

---
+++ mod-1/src/main/kotlin/kotx/customgui/util/JsonBuilder.kt
@@ -0,0 +1,66 @@
+package kotx.customgui.util
+
+import kotlinx.serialization.json.*
+
+class JsonBuilder {
+    private var content = mutableMapOf<String, JsonElement>()
+
+    infix fun String.to(value: String?) {
+        content[this] = JsonPrimitive(value)
+    }
+
+    infix fun String.to(value: Number?) {
+        content[this] = JsonPrimitive(value)
+    }
+
+    infix fun String.to(value: Boolean?) {
+        content[this] = JsonPrimitive(value)
+    }
+
+    infix fun String.to(value: JsonElement) {
+        content[this] = value
+    }
+
+    infix fun String.to(values: Collection<JsonElement>) {
+        values.forEach { this to it }
+    }
+
+    infix fun String.to(builder: JsonBuilder.() -> Unit) {
+        content[this] = JsonBuilder().apply(builder).build()
+    }
+
+    infix fun String.array(builder: JsonArrayBuilder.() -> Unit) {
+        content[this] = JsonArrayBuilder().apply(builder).build()
+    }
+
+    fun build() = JsonObject(content)
+}
+
+class JsonArrayBuilder {
+    private var contents = mutableListOf<JsonElement>()
+
+    operator fun String?.unaryPlus() {
+        contents.add(JsonPrimitive(this))
+    }
+
+    operator fun Number?.unaryPlus() {
+        contents.add(JsonPrimitive(this))
+    }
+
+    operator fun Boolean?.unaryPlus() {
+        contents.add(JsonPrimitive(this))
+    }
+
+    operator fun JsonElement.unaryPlus() {
+        contents.add(this)
+    }
+
+    operator fun Collection<JsonElement>.unaryPlus() {
+        contents.addAll(this)
+    }
+
+    fun build() = JsonArray(contents)
+}
+
+fun json(block: JsonBuilder.() -> Unit) = JsonBuilder().apply(block).build()
+fun jsonArray(block: JsonArrayBuilder.() -> Unit) = JsonArrayBuilder().apply(block).build()
