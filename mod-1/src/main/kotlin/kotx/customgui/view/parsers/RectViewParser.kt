aba45503873de44078f35dee199c4a94849812b6
kotx <developer.hazuku@gmail.com>
2021-06-30 00:32:27 +0900

    ✨ New: RectView parser

---
+++ mod-1/src/main/kotlin/kotx/customgui/view/parsers/RectViewParser.kt
@@ -0,0 +1,25 @@
+package kotx.customgui.view.parsers
+
+import kotlinx.serialization.json.*
+import kotx.customgui.util.*
+import kotx.customgui.view.*
+import kotx.customgui.view.views.*
+import java.awt.*
+
+class RectViewParser : ViewParser<RectView> {
+    override fun encode(view: RectView): JsonObject = json {
+        "color" to view.color.rgb
+        "x1" to view.x1
+        "y1" to view.y1
+        "x2" to view.x2
+        "y2" to view.y2
+    }
+
+    override fun decode(json: JsonObject): RectView = RectView(
+        Color(json.getInt("color")),
+        json.getInt("x1"),
+        json.getInt("y1"),
+        json.getInt("x2"),
+        json.getInt("y2"),
+    )
+}
