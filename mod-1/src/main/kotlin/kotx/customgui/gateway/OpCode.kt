33dc2c792c3d68ae66b33d79b6f47faec62a2d58
kotx <developer.hazuku@gmail.com>
2021-06-30 00:30:51 +0900

    ✨ New: OpCodes

---
+++ mod-1/src/main/kotlin/kotx/customgui/gateway/OpCode.kt
@@ -0,0 +1,12 @@
+package kotx.customgui.gateway
+
+enum class OpCode(val value: Int) {
+    SAVE_GUI(1),
+    LOAD_GUI(2),
+    SHOW_GUI(3),
+    UNKNOWN(-1);
+
+    companion object {
+        fun get(opCode: Int) = values().find { it.value == opCode } ?: UNKNOWN
+    }
+}
