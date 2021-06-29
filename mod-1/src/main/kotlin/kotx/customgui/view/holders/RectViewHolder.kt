a0385889e86b993a7e6d69343688553d4ec4e0cd
kotx <developer.hazuku@gmail.com>
2021-06-30 04:30:07 +0900

    Fix dependencies, keep it simple

---
+++ mod-1/src/main/kotlin/kotx/customgui/view/holders/RectViewHolder.kt
@@ -0,0 +1,6 @@
+package kotx.customgui.view.holders
+
+import kotx.customgui.view.*
+import kotx.customgui.view.views.*
+
+class RectViewHolder(override val index: Int, override val type: ViewType, override val view: RectView) : ViewHolder
