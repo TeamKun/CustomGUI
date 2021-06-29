7019fc075d494041d18c15d9df91f7436b276a88
kotx <developer.hazuku@gmail.com>
2021-06-30 04:29:38 +0900

    wip

--- mod-1/src/main/kotlin/kotx/customgui/gateway/handlers/LoadGUIHandler.kt
+++ mod-1/src/main/kotlin/kotx/customgui/gateway/handlers/LoadGUIHandler.kt
@@ -5,12 +5,19 @@ import kotx.customgui.*
 import kotx.customgui.gateway.*
 import kotx.customgui.gui.guis.*
 import kotx.customgui.util.*
+import kotx.customgui.view.*
 
 class LoadGUIHandler : GatewayHandler {
     override val opCode: OpCode = OpCode.LOAD_GUI
 
     override fun handle(json: JsonObject) {
+        val guis = json.getObjectArray("guis").map {
+            val type = ViewType.get(it.getInt("type"))
+            val parser = CustomGUIMod.viewHandler.getParser(type)
+            parser.decode(it)
+        }
+
         EditorGUI.holders.clear()
-        json.getObjectArray("guis").mapNotNull { CustomGUIMod.getParser(json.getInt("type"))?.decode(json) }.also { EditorGUI.holders.addAll(it) }
+        EditorGUI.holders.addAll(guis)
     }
 }
