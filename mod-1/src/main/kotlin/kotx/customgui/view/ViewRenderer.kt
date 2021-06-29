4ef89fed0eb06f65c0873fe8f5b0104cf977af3b
kotx <developer.hazuku@gmail.com>
2021-06-30 04:33:39 +0900

    fix matrix stack

--- mod-1/src/main/kotlin/kotx/customgui/view/ViewRenderer.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/ViewRenderer.kt
@@ -1,6 +1,8 @@
 package kotx.customgui.view
 
+import com.mojang.blaze3d.matrix.*
+
 interface ViewRenderer {
-    fun renderPreview(x1: Int, y1: Int, x2: Int, y2: Int, view: View)
-    fun renderFull(x1: Int, y1: Int, x2: Int, y2: Int, view: View)
+    fun renderPreview(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: View)
+    fun renderFull(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: View)
 }
--- mod-1/src/main/kotlin/kotx/customgui/view/renderers/TextViewRenderer.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/renderers/TextViewRenderer.kt
@@ -1,13 +1,14 @@
 package kotx.customgui.view.renderers
 
+import com.mojang.blaze3d.matrix.*
 import kotx.customgui.view.*
 
 class TextViewRenderer : ViewRenderer {
-    override fun renderPreview(x1: Int, y1: Int, x2: Int, y2: Int, view: View) {
+    override fun renderPreview(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: View) {
 
     }
 
-    override fun renderFull(x1: Int, y1: Int, x2: Int, y2: Int, view: View) {
+    override fun renderFull(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: View) {
 
     }
 }
