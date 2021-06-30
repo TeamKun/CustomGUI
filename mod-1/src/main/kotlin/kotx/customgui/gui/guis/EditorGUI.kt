c4f0cfcf8d2e827db0dcdbf8c4df942ca17ef899
kotx <developer.hazuku@gmail.com>
2021-06-30 23:42:57 +0900

    wip

--- mod-1/src/main/kotlin/kotx/customgui/gui/guis/EditorGUI.kt
+++ mod-1/src/main/kotlin/kotx/customgui/gui/guis/EditorGUI.kt
@@ -5,15 +5,31 @@ import kotx.customgui.gui.*
 import kotx.customgui.view.*
 import kotx.customgui.view.renderers.*
 import kotx.customgui.view.views.*
+import java.awt.*
 
 object EditorGUI : GUI() {
     val holders = mutableListOf<ViewHolder>()
+
+    private const val editorWidth = 360
+    private const val editorHeight = 203
+
+    override fun initialize() {
+        button("Text", 10, 10)
+    }
+
     override fun draw(stack: MatrixStack, mouseX: Int, mouseY: Int) {
+        //Dimmed background
+        rect(stack, 0, 0, width, height, Color(0, 0, 0, 100))
+        //Editor background
+        rectCenter(stack, width / 2, height / 2, editorWidth, editorHeight, Color(12, 12, 12))
+
+
         holders.sortedBy { it.index }.forEach {
-            val renderer = it.view.renderer
+            val renderer = it.content.renderer
             when (it.type) {
-                ViewType.TEXT -> (renderer as TextViewRenderer).renderPreview(stack, it.view.x1, it.view.y1, it.view.x2, it.view.y2, it.view as TextView)
-                ViewType.RECT -> (renderer as RectViewRenderer).renderPreview(stack, it.view.x1, it.view.y1, it.view.x2, it.view.y2, it.view as RectView)
+                ViewType.TEXT -> (renderer as TextViewRenderer).renderPreview(stack, it.content.x1, it.content.y1, it.content.x2, it.content.y2, it.content as TextView)
+                ViewType.RECT -> (renderer as RectViewRenderer).renderPreview(stack, it.content.x1, it.content.y1, it.content.x2, it.content.y2, it.content as RectView)
+                ViewType.BUTTON -> (renderer as ButtonViewRenderer).renderPreview(stack, it.content.x1, it.content.y1, it.content.x2, it.content.y2, it.content as ButtonView)
             }
         }
     }
