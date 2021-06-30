f6adcafeb3972f86367c205b7af2ea7ec30c6460
kotx <developer.hazuku@gmail.com>
2021-06-30 23:42:02 +0900

    ✨ New: buttonview holder

---
+++ mod-1/src/main/kotlin/kotx/customgui/view/holders/ButtonViewHolder.kt
@@ -0,0 +1,6 @@
+package kotx.customgui.view.holders
+
+import kotx.customgui.view.*
+import kotx.customgui.view.views.*
+
+class ButtonViewHolder(override val index: Int, override val type: ViewType, override val content: ButtonView) : ViewHolder
---
+++ mod-1/src/main/kotlin/kotx/customgui/view/holders/parsers/ButtonViewHolderParser.kt
@@ -0,0 +1,25 @@
+package kotx.customgui.view.holders.parsers
+
+import kotlinx.serialization.json.*
+import kotx.customgui.util.*
+import kotx.customgui.view.*
+import kotx.customgui.view.holders.*
+import kotx.customgui.view.parsers.*
+import kotx.customgui.view.views.*
+
+class ButtonViewHolderParser : ViewHolderParser<ButtonViewHolder> {
+    override val type: ViewType = ViewType.BUTTON
+    override val viewParser: ViewParser<ButtonView> = ButtonViewParser()
+
+    override fun encode(holder: ButtonViewHolder): JsonObject = json {
+        "type" to type.value
+        "index" to holder.index
+        "content" to viewParser.encode(holder.content)
+    }
+
+    override fun decode(json: JsonObject): ButtonViewHolder = ButtonViewHolder(
+        json.getInt("index"),
+        ViewType.get(json.getInt("type")),
+        viewParser.decode(json.getObject("content"))
+    )
+}
