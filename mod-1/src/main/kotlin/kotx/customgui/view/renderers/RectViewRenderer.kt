a7568466261e6205b7be90f8e1043e3e28d4e8e8
kotx <developer.hazuku@gmail.com>
2021-06-30 23:44:56 +0900

    Display rectangle color information as text during preview

--- mod-1/src/main/kotlin/kotx/customgui/view/renderers/RectViewRenderer.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/renderers/RectViewRenderer.kt
@@ -4,10 +4,14 @@ import com.mojang.blaze3d.matrix.*
 import kotx.customgui.gui.*
 import kotx.customgui.view.*
 import kotx.customgui.view.views.*
+import java.awt.*
 
 class RectViewRenderer : ViewRenderer<RectView> {
     override fun renderPreview(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: RectView) {
-        GUI.rect(stack, x1, y1, x2, y2, view.color)
+        val color = view.color
+
+        GUI.rect(stack, x1, y1, x2, y2, color)
+        GUI.textShadow(stack, "R:${color.red} | G:${color.green} | B:${color.blue}", x1, y2, Color.WHITE)
     }
 
     override fun renderFull(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: RectView) {
