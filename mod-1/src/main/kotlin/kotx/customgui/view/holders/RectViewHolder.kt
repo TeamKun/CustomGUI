7eee4c437fdeaed05c11d8562a2739c3138a860c
kotx <developer.hazuku@gmail.com>
2021-06-30 23:44:17 +0900

    view -> content

--- mod-1/src/main/kotlin/kotx/customgui/view/holders/RectViewHolder.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/holders/RectViewHolder.kt
@@ -3,4 +3,4 @@ package kotx.customgui.view.holders
 import kotx.customgui.view.*
 import kotx.customgui.view.views.*
 
-class RectViewHolder(override val index: Int, override val type: ViewType, override val view: RectView) : ViewHolder
+class RectViewHolder(override val index: Int, override val type: ViewType, override val content: RectView) : ViewHolder
