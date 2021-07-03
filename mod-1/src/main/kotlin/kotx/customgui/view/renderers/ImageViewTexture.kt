acd248b572ddabe5f3c01cb5288f2ad0a0597bd8
kotlin <developer.hazuku@gmail.com>
2021-07-04 03:37:18 +0900

    new texture object for image view

---
+++ mod-1/src/main/kotlin/kotx/customgui/view/renderers/ImageViewTexture.kt
@@ -0,0 +1,64 @@
+package kotx.customgui.view.renderers
+
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.*
+import kotx.customgui.util.*
+import kotx.customgui.view.views.*
+import net.minecraft.client.renderer.texture.*
+import net.minecraft.resources.*
+import net.minecraft.util.*
+import org.apache.commons.io.*
+import java.io.*
+
+class ImageViewTexture(
+    val view: ImageView,
+    resourceLocation: ResourceLocation
+) : SimpleTexture(resourceLocation), CoroutineScope {
+    override val coroutineContext = Dispatchers.Default
+
+    private val client = HttpClient {
+        expectSuccess = false
+        BrowserUserAgent()
+    }
+
+    override fun loadTexture(manager: IResourceManager) {
+        val cacheFile = File("./mods/CustomGUI/caches")
+        cacheFile.parentFile.mkdirs()
+
+        if (cacheFile.exists()) {
+            loadImage(cacheFile)
+            return
+        }
+
+        launch {
+            FileUtils.copyInputStreamToFile(client.get<HttpStatement>(view.url).receive(), cacheFile)
+
+            MainThreadExecutor.offer {
+                loadImage(cacheFile)
+            }
+        }
+    }
+
+    private fun loadImage(cacheFile: File) {
+        TextureUtil.prepareImage(getGlTextureId(), 0, view.width, view.height)
+        NativeImage.read(cacheFile.inputStream()).uploadTextureSub(
+            0,
+            0,
+            0,
+            0,
+            0,
+            view.width,
+            view.height,
+            blur,
+            false,
+            false,
+            false
+        )
+
+        view.isLoading = false
+        view.isLoaded = true
+    }
+}
