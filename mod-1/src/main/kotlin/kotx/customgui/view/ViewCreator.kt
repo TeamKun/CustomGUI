8eda12a66d39e2e918ab5b9141870e4daf16f36a
Kotx <developer.hazuku@gmail.com>
2021-07-05 01:43:06 +0900

    implemented

--- mod-1/src/main/kotlin/kotx/customgui/view/ViewCreator.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/ViewCreator.kt
@@ -1,7 +1,7 @@
 package kotx.customgui.view
 
 import kotx.customgui.gui.*
-import kotx.customgui.gui.guis.*
+import kotx.customgui.gui.guis.editor.*
 import kotx.customgui.view.holders.*
 import kotx.customgui.view.views.*
 
@@ -9,15 +9,31 @@ abstract class ViewCreator<T : View> : GUI() {
     abstract val type: ViewType
     abstract val points: Int
 
+    var x1: Int = 0
+    var y1: Int = 0
+    var x2: Int? = null
+    var y2: Int? = null
+
     protected fun build(view: T) {
+        val index = (EditorGUI.holders.maxByOrNull { it.index }?.index ?: 0) + 1
         val holder = when (view) {
-            is ButtonView -> ButtonViewHolder(1, view)
+            is TextView -> TextViewHolder(index, view)
+            is RectView -> RectViewHolder(index, view)
+            is ButtonView -> ButtonViewHolder(index, view)
+            is ImageView -> ImageViewHolder(index, view)
             else -> null
         }
 
+        view.x1 = x1
+        view.y1 = y1
+        x2?.also { view.x2 = it }
+        y2?.also { view.y2 = it }
 
         if (holder != null)
             EditorGUI.holders.add(holder)
+
+        EditorGUI.selectingCreator = -1
+        EditorGUI.lastLocation = null
 
         display(EditorGUI)
     }
