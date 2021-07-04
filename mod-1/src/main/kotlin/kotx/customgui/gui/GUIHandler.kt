4b1808c4d7808d811727cb69e381717dc16c2f92
Kotx <developer.hazuku@gmail.com>
2021-07-05 01:45:11 +0900

    removed mouseClicked, add mouseDragDiff, add onClose

--- mod-1/src/main/kotlin/kotx/customgui/gui/GUIHandler.kt
+++ mod-1/src/main/kotlin/kotx/customgui/gui/GUIHandler.kt
@@ -21,15 +21,27 @@ class GUIHandler(private val gui: GUI) : Screen("GuiHandler".component()) {
 
     private val pressingButtons = mutableListOf<MouseButton>()
 
+    var lastX: Int? = null
+    var lastY: Int? = null
+
     override fun mouseMoved(mouseX: Double, mouseY: Double) {
         val x = mouseX.toInt()
         val y = mouseY.toInt()
 
         gui.onMouseMove(x, y)
 
+        val diffX = lastX?.let { x - it }
+        val diffY = lastY?.let { y - it }
+
         pressingButtons.forEach {
             gui.onMouseDrag(it, x, y)
+
+            if (diffX != null && diffY != null)
+                gui.onMouseDragDiff(it, diffX, diffY)
         }
+
+        lastX = x
+        lastY = y
 
         super.mouseMoved(mouseX, mouseY)
     }
@@ -44,13 +56,9 @@ class GUIHandler(private val gui: GUI) : Screen("GuiHandler".component()) {
     }
 
     override fun mouseReleased(mouseX: Double, mouseY: Double, button: Int): Boolean {
-        val mouseButton = MouseButton.get(button)
-
-        gui.onMouseRelease(mouseButton, mouseX.toInt(), mouseY.toInt())
-        if (pressingButtons.find { it.button == button } != null)
-            gui.onMouseClick(mouseButton, mouseX.toInt(), mouseY.toInt())
-
         pressingButtons.removeIf { it.button == button }
+
+        gui.onMouseRelease(MouseButton.get(button), mouseX.toInt(), mouseY.toInt())
 
         return super.mouseReleased(mouseX, mouseY, button)
     }
@@ -65,6 +73,12 @@ class GUIHandler(private val gui: GUI) : Screen("GuiHandler".component()) {
         gui.onKeyRelease(keyCode, modifiers)
 
         return super.keyReleased(keyCode, scanCode, modifiers)
+    }
+
+    override fun onClose() {
+        gui.onClose()
+
+        super.onClose()
     }
 
     fun button(text: String, x: Int, y: Int, width: Int, height: Int, onClick: Button.() -> Unit) = Button(
