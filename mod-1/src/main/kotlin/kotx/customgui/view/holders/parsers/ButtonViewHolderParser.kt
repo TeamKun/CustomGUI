804ab776ebc67c2fdb521ee4d085188eec3f2adc
Kotx <developer.hazuku@gmail.com>
2021-07-15 23:33:09 +0900

    Remove Type

--- mod-1/src/main/kotlin/kotx/customgui/view/holders/parsers/ButtonViewHolderParser.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/holders/parsers/ButtonViewHolderParser.kt
@@ -1,15 +1,17 @@
 package kotx.customgui.view.holders.parsers
 
-import kotlinx.serialization.json.*
-import kotx.customgui.util.*
-import kotx.customgui.view.*
-import kotx.customgui.view.holders.*
-import kotx.customgui.view.parsers.*
-import kotx.customgui.view.views.*
+import kotlinx.serialization.json.JsonObject
+import kotx.customgui.util.getInt
+import kotx.customgui.util.getObject
+import kotx.customgui.util.json
+import kotx.customgui.view.ViewHolderParser
+import kotx.customgui.view.ViewType
+import kotx.customgui.view.holders.ButtonViewHolder
+import kotx.customgui.view.parsers.ButtonViewParser
 
 class ButtonViewHolderParser : ViewHolderParser<ButtonViewHolder> {
     override val type: ViewType = ViewType.BUTTON
-    override val viewParser: ViewParser<ButtonView> = ButtonViewParser()
+    override val viewParser = ButtonViewParser()
 
     override fun encode(holder: ButtonViewHolder): JsonObject = json {
         "type" to type.value
