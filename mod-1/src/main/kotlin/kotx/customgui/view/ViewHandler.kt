1b1384d74b55da06745bbbbe16337f5d330bff8f
Kotx <developer.hazuku@gmail.com>
2021-07-16 00:15:39 +0900

    aaaa

--- mod-1/src/main/kotlin/kotx/customgui/view/ViewHandler.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/ViewHandler.kt
@@ -1,12 +1,49 @@
 package kotx.customgui.view
 
-import kotx.customgui.view.holders.parsers.*
+import kotlinx.serialization.json.JsonObject
+import kotx.customgui.util.getInt
+import kotx.customgui.view.holders.ButtonViewHolder
+import kotx.customgui.view.holders.ImageViewHolder
+import kotx.customgui.view.holders.RectViewHolder
+import kotx.customgui.view.holders.TextViewHolder
+import kotx.customgui.view.holders.parsers.ButtonViewHolderParser
+import kotx.customgui.view.holders.parsers.ImageViewHolderParser
+import kotx.customgui.view.holders.parsers.RectViewHolderParser
+import kotx.customgui.view.holders.parsers.TextViewHolderParser
 
 class ViewHandler {
     private val parsers = listOf(
         TextViewHolderParser(),
         RectViewHolderParser(),
+        ImageViewHolderParser(),
+        ButtonViewHolderParser(),
     )
 
     fun getParser(type: ViewType) = parsers.find { it.type == type }!!
+    fun getParserByHolder(holder: ViewHolder) = getParser(
+        when (holder) {
+            is TextViewHolder -> ViewType.TEXT
+            is RectViewHolder -> ViewType.RECT
+            is ImageViewHolder -> ViewType.IMAGE
+            is ButtonViewHolder -> ViewType.BUTTON
+            else -> null
+        }!!
+    )
+
+    fun encode(holder: ViewHolder): JsonObject {
+        val parser = getParserByHolder(holder)
+        return when (parser) {
+            is TextViewHolderParser -> parser.encode(holder as TextViewHolder)
+            is RectViewHolderParser -> parser.encode(holder as RectViewHolder)
+            is ImageViewHolderParser -> parser.encode(holder as ImageViewHolder)
+            is ButtonViewHolderParser -> parser.encode(holder as ButtonViewHolder)
+            else -> null
+        }!!
+    }
+
+    fun decode(json: JsonObject): ViewHolder {
+        val type = ViewType.get(json.getInt("type"))
+        val parser = getParser(type)
+        return parser.decode(json)
+    }
 }
