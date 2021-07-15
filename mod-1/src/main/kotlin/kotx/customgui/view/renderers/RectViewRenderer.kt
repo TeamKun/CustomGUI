8819e3879635644da55c89381dad4d9883697858
Kotx <developer.hazuku@gmail.com>
2021-07-16 00:14:50 +0900

    rm info

--- mod-1/src/main/kotlin/kotx/customgui/view/renderers/RectViewRenderer.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/renderers/RectViewRenderer.kt
@@ -1,17 +1,15 @@
 package kotx.customgui.view.renderers
 
-import com.mojang.blaze3d.matrix.*
-import kotx.customgui.gui.*
-import kotx.customgui.view.*
-import kotx.customgui.view.views.*
-import java.awt.*
+import com.mojang.blaze3d.matrix.MatrixStack
+import kotx.customgui.gui.GUI
+import kotx.customgui.view.ViewRenderer
+import kotx.customgui.view.views.RectView
 
 class RectViewRenderer : ViewRenderer<RectView> {
     override fun renderPreview(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: RectView) {
         val color = view.color
 
         GUI.rect(stack, x1, y1, x2, y2, color)
-        GUI.textShadow(stack, "R:${color.red} | G:${color.green} | B:${color.blue}", x1, y2, Color.WHITE)
     }
 
     override fun renderFull(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: RectView) {
