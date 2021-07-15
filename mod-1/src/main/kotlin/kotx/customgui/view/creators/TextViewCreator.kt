710470cc560fca84e9fbdd26f58ec3a7cab2f93d
Kotx <developer.hazuku@gmail.com>
2021-07-16 00:15:02 +0900

    wi

--- mod-1/src/main/kotlin/kotx/customgui/view/creators/TextViewCreator.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/creators/TextViewCreator.kt
@@ -1,17 +1,22 @@
 package kotx.customgui.view.creators
 
-import com.mojang.blaze3d.matrix.*
-import kotx.customgui.util.*
-import kotx.customgui.view.*
-import kotx.customgui.view.views.*
-import java.awt.*
+import com.mojang.blaze3d.matrix.MatrixStack
+import kotx.customgui.util.fontRenderer
+import kotx.customgui.view.ViewCreator
+import kotx.customgui.view.ViewType
+import kotx.customgui.view.views.TextView
+import net.minecraft.client.gui.widget.TextFieldWidget
+import org.lwjgl.glfw.GLFW
+import java.awt.Color
 
 class TextViewCreator : ViewCreator<TextView>() {
     override val type: ViewType = ViewType.TEXT
     override val points: Int = 1
 
+    private lateinit var textField: TextFieldWidget
+
     override fun initialize() {
-        val textField = textFieldCenter("テキスト", width / 2, 50, 200, fontRenderer.FONT_HEIGHT + 11).apply {
+        textField = textFieldCenter("テキスト", width / 2, 50, 200, fontRenderer.FONT_HEIGHT + 11).apply {
             setCanLoseFocus(true)
             setMaxStringLength(1024)
             setFocused2(true)
@@ -19,8 +24,8 @@ class TextViewCreator : ViewCreator<TextView>() {
 
         val button = buttonCenter("作成", width / 2, 100) {
             build(TextView(textField.text, Color.WHITE).apply {
-                this.x2 = this@TextViewCreator.x1 + fontRenderer.getStringWidth(textField.text)
-                this.y2 = this@TextViewCreator.y1 + fontRenderer.FONT_HEIGHT
+                this@TextViewCreator.x2 = this@TextViewCreator.x1 + fontRenderer.getStringWidth(textField.text)
+                this@TextViewCreator.y2 = this@TextViewCreator.y1 + fontRenderer.FONT_HEIGHT
             })
         }
 
@@ -29,6 +34,16 @@ class TextViewCreator : ViewCreator<TextView>() {
         }
 
         button.active = false
+    }
+
+    override fun onKeyPress(key: Int, modifiers: Int): Boolean {
+        if (key == GLFW.GLFW_KEY_ENTER && textField.text.isNotBlank())
+            build(TextView(textField.text, Color.WHITE).apply {
+                this@TextViewCreator.x2 = this@TextViewCreator.x1 + fontRenderer.getStringWidth(textField.text)
+                this@TextViewCreator.y2 = this@TextViewCreator.y1 + fontRenderer.FONT_HEIGHT
+            })
+
+        return true
     }
 
     override fun draw(stack: MatrixStack, mouseX: Int, mouseY: Int) {
