f2d0b3eb882241a7312444c3978b4aa2dcf5ae87
Kotx <developer.hazuku@gmail.com>
2021-07-17 23:27:48 +0900

    add handleClick(wip), remove "/" from preview to To enable chat input

--- mod-1/src/main/kotlin/kotx/customgui/view/renderers/ButtonViewRenderer.kt
+++ mod-1/src/main/kotlin/kotx/customgui/view/renderers/ButtonViewRenderer.kt
@@ -1,19 +1,24 @@
 package kotx.customgui.view.renderers
 
-import com.mojang.blaze3d.matrix.*
-import kotx.customgui.gui.*
-import kotx.customgui.view.*
-import kotx.customgui.view.views.*
-import java.awt.*
+import com.mojang.blaze3d.matrix.MatrixStack
+import kotx.customgui.gui.GUI
+import kotx.customgui.gui.MouseButton
+import kotx.customgui.view.ViewRenderer
+import kotx.customgui.view.views.ButtonView
+import java.awt.Color
 
 class ButtonViewRenderer : ViewRenderer<ButtonView> {
     override fun renderPreview(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: ButtonView) {
-        val cmd = "/" + view.command.replaceFirst("^/".toRegex(), "")
+        val cmd = view.command
         GUI.rect(stack, x1, y1, x2, y2, Color(0, 0, 255, 100))
         GUI.text(stack, cmd, x1, y2, Color.WHITE, true)
     }
 
     override fun renderFull(stack: MatrixStack, x1: Int, y1: Int, x2: Int, y2: Int, view: ButtonView) {
+
+    }
+
+    override fun handleClick(button: MouseButton, x: Int, y: Int) {
 
     }
 }
