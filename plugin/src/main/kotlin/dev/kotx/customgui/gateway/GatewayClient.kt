801c26e59af4bcf9259da7989925951583ffe4da
Kotx <developer.hazuku@gmail.com>
2021-07-19 04:03:34 +0900

    new GatewayClient

---
+++ plugin/src/main/kotlin/dev/kotx/customgui/gateway/GatewayClient.kt
@@ -0,0 +1,36 @@
+package dev.kotx.customgui.gateway
+
+import dev.kotx.customgui.asJsonObject
+import dev.kotx.customgui.json
+import kotlinx.serialization.json.JsonObject
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.plugin.messaging.PluginMessageListener
+import java.io.ByteArrayOutputStream
+import java.io.DataOutputStream
+
+class GatewayClient(
+    private val plugin: JavaPlugin
+) : PluginMessageListener {
+    override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray) {
+        val msg = message.drop(1).dropLast(1).toByteArray().toString(Charsets.UTF_8)
+        val json = msg.asJsonObject()
+    }
+
+    fun send(player: Player, opCode: OpCode, data: JsonObject) {
+        player.sendPluginMessage(plugin, "customgui:messenger", json {
+            "op" to opCode.value
+            "data" to data
+        }.toString().asPacket())
+    }
+
+    private fun String.asPacket(): ByteArray {
+        val baos = ByteArrayOutputStream()
+        val out = DataOutputStream(baos)
+        out.writeByte(0)
+        out.writeBytes(this)
+        out.writeByte(0)
+
+        return baos.toByteArray()
+    }
+}
