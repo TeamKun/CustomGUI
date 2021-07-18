f5c418184afce09f0ebb638d5930ded264ee56dd
Kotx <developer.hazuku@gmail.com>
2021-07-19 03:56:39 +0900

    remove old mod files

--- plugin/src/main/kotlin/kotx/minecraft/plugins/customgui/directory/Directories.kt
+++
@@ -1,7 +0,0 @@
-package kotx.minecraft.plugins.customgui.directory
-
-import java.nio.file.Paths
-
-object Directories {
-    val guis = DirectoryHandler(Paths.get("plugins", "CustomGUI", "gui").toFile())
-}
