3dd33ec91cb513505f7b9caae9b81e335693cc76
Kotx <developer.hazuku@gmail.com>
2021-07-20 02:17:17 +0900

    adds player

--- plugin/src/main/kotlin/dev/kotx/customgui/gateway/GatewayHandler.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/gateway/GatewayHandler.kt
@@ -1,9 +1,10 @@
 package dev.kotx.customgui.gateway
 
 import kotlinx.serialization.json.JsonObject
+import org.bukkit.entity.Player
 
 interface GatewayHandler {
     val opCode: OpCode
 
-    fun handle(json: JsonObject)
+    fun handle(player: Player, json: JsonObject)
 }
