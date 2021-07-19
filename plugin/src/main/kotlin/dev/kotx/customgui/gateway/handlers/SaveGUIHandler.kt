884dbf7eede09ab9fbce320066b068dab8f704a5
Kotx <developer.hazuku@gmail.com>
2021-07-20 02:17:22 +0900

    implemented

--- plugin/src/main/kotlin/dev/kotx/customgui/gateway/handlers/SaveGUIHandler.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/gateway/handlers/SaveGUIHandler.kt
@@ -3,11 +3,23 @@ package dev.kotx.customgui.gateway.handlers
 import dev.kotx.customgui.gateway.GatewayHandler
 import dev.kotx.customgui.gateway.OpCode
 import kotlinx.serialization.json.JsonObject
+import org.bukkit.entity.Player
+import java.io.File
 
 class SaveGUIHandler : GatewayHandler {
     override val opCode: OpCode = OpCode.SAVE_GUI
+    private val workspaceDirectory = File("./plugins/CustomGUI/workspaces/")
 
-    override fun handle(json: JsonObject) {
+    override fun handle(player: Player, json: JsonObject) {
+        val playerWorkspaceFile = File(workspaceDirectory, "${player.uniqueId}.json")
+        if (!workspaceDirectory.exists())
+            workspaceDirectory.mkdirs()
 
+        if (playerWorkspaceFile.exists())
+            playerWorkspaceFile.delete()
+
+        playerWorkspaceFile.createNewFile()
+
+        playerWorkspaceFile.writeText(json.toString())
     }
 }
