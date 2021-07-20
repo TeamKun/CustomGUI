b65be5e4c63ca03523df997df7d0f8866f975437
Kotx <developer.hazuku@gmail.com>
2021-07-21 01:59:48 +0900

    All file management integrated into Files

--- plugin/src/main/kotlin/dev/kotx/customgui/Files.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/Files.kt
@@ -12,9 +12,23 @@ object Files {
     fun init(plugin: JavaPlugin) {
         plugin.server.scheduler.scheduleSyncRepeatingTask(plugin, {
             guiDirectory.allFiles().map {
-                GUI(it.parentFile.name, it.name, it.readText().asJsonArray())
+                GUI(it.parentFile.name, it.nameWithoutExtension, it.readText().asJsonArray())
             }
         }, 0, 3000)
+    }
+
+    fun save(gui: GUI) {
+        val guiFile = File(guiDirectory, "${gui.author}/${gui.name}.json")
+        if (!guiFile.parentFile.exists())
+            guiFile.parentFile.mkdirs()
+        if (!guiFile.exists())
+            guiFile.createNewFile()
+
+        guiFile.writeText(gui.views.toString())
+    }
+
+    fun findByName(name: String) = guiDirectory.allFiles().find { it.nameWithoutExtension == name }?.let {
+        GUI(it.parentFile.name, it.nameWithoutExtension, it.readText().asJsonArray())
     }
 
     private fun File.allFiles(): List<File> = listFiles().flatMap { if (it.isDirectory) it.allFiles() else listOf(it) }
