3787f0ec614b48448a6c20cb84a95ef0508c2cc9
Kotx <developer.hazuku@gmail.com>
2021-07-21 01:59:21 +0900

    wip

--- plugin/src/main/kotlin/dev/kotx/customgui/commands/AddCommand.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/commands/AddCommand.kt
@@ -1,13 +1,17 @@
 package dev.kotx.customgui.commands
 
+import dev.kotx.customgui.CustomGUIListener
 import dev.kotx.customgui.Files
+import dev.kotx.customgui.GUI
 import dev.kotx.customgui.asJsonObject
 import dev.kotx.customgui.getArray
 import dev.kotx.flylib.command.Command
 import dev.kotx.flylib.command.CommandContext
+import net.kyori.adventure.text.TextComponent
 import org.bukkit.entity.Player
 import java.awt.Color
 import java.io.File
+import java.util.*
 
 class AddCommand : Command("add") {
     init {
@@ -36,20 +40,84 @@ class AddCommand : Command("add") {
         if (!normalizer.matches(args.first())) {
             pluginMessage {
                 bold(args.first(), Color.RED)
-                append("は使用できない文字が含まれています。ファイル名には半角英数字とハイフン、アンダースコアを使用することが出来ます。")
+                append("は使用できない文字が含まれています。GUI名には半角英数字とハイフン、アンダースコアを使用することが出来ます。")
             }
             return
         }
 
-        if (Files.guis.any { it.name == args.first() }) {
-            Files.guis.find { it.name == args.first() }!!.author
-            pluginMessage {
+        val gui = Files.findByName(args.first())
+        if (gui != null) {
+            val author = server.getOfflinePlayer(UUID.fromString(gui.author))
+            if (gui.author == player!!.uniqueId.toString() || player!!.hasPermission("customgui.gui.delete")) {
+                pluginMessage {
+                    bold(args.first())
+                    append("という名前のGUIは")
+                    bold(author.name ?: "<UnknownPlayer>")
+                    appendln("が既に作成しています。上書きしますか？")
+                    append("チャットに入力")
+                    appendln(":", Color.LIGHT_GRAY)
+                    append("はい", Color.GREEN)
+                    append(":", Color.LIGHT_GRAY)
+                    boldln("Yes(Y)", Color.GREEN)
+                    append("いいえ", Color.RED)
+                    append(":", Color.LIGHT_GRAY)
+                    bold("No(N)", Color.RED)
+                }
 
+                CustomGUIListener.wait(player!!) {
+                    val message = (it.message() as TextComponent).content()
+                    when (message.lowercase()) {
+                        "yes", "y", "はい" -> {
+                            val playerWorkspaceJson = playerWorkspaceFile.readText().asJsonObject()
+                            val views = playerWorkspaceJson.getArray("views")
+
+                            Files.save(
+                                GUI(
+                                    player!!.uniqueId.toString(),
+                                    player!!.name,
+                                    views
+                                )
+                            )
+
+                            pluginMessage {
+                                bold(args.first(), Color.GREEN)
+                                append("を更新しました。")
+                            }
+                        }
+
+                        else -> {
+                            pluginMessage {
+                                bold(args.first(), Color.RED)
+                                append("の更新をキャンセルしました。")
+                            }
+                        }
+                    }
+                }
+            } else {
+                pluginMessage {
+                    bold(args.first(), Color.RED)
+                    append("という名前のGUIは", Color.RED)
+                    bold(author.name ?: "<UnknownPlayer>", Color.RED)
+                    append("が既に作成しています。作成されていないGUI名を使用して下さい。", Color.RED)
+                }
             }
             return
         }
 
         val playerWorkspaceJson = playerWorkspaceFile.readText().asJsonObject()
         val views = playerWorkspaceJson.getArray("views")
+
+        Files.save(
+            GUI(
+                player!!.uniqueId.toString(),
+                player!!.name,
+                views
+            )
+        )
+
+        pluginMessage {
+            bold(args.first(), Color.GREEN)
+            append("という名前でGUIを保存しました。")
+        }
     }
 }
