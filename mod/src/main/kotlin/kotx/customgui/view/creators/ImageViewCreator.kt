9b6c7adbd8b321b417c5df04c18b7975906be41e
Kotx <developer.hazuku@gmail.com>
2021-07-21 03:34:17 +0900

    fix url regex

--- mod/src/main/kotlin/kotx/customgui/view/creators/ImageViewCreator.kt
+++ mod/src/main/kotlin/kotx/customgui/view/creators/ImageViewCreator.kt
@@ -64,7 +64,7 @@ class ImageViewCreator : ViewCreator<ImageView, ImageViewHolder>(), CoroutineSco
         }
 
         val urlRegex =
-            "\"((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w\\-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[.\\!\\/\\\\w]*))?)\"ig".toRegex(
+            "((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w\\-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[.\\!\\/\\\\w]*))?)".toRegex(
                 RegexOption.IGNORE_CASE
             )
         textField.setResponder {
