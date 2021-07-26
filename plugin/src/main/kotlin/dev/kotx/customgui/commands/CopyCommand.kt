f2667f4594f7aa0b420f77dfa3ecb56b3f8c8d6a
Kotx <developer.hazuku@gmail.com>
2021-07-27 02:58:45 +0900

    implement Copycommand

--- plugin/src/main/kotlin/dev/kotx/customgui/commands/CopyCommand.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/commands/CopyCommand.kt
@@ -1,9 +1,125 @@
 package dev.kotx.customgui.commands
 
+import dev.kotx.customgui.CustomGUIListener
+import dev.kotx.customgui.Files
+import dev.kotx.customgui.GUI
 import dev.kotx.flylib.command.Command
+import dev.kotx.flylib.command.CommandContext
+import net.kyori.adventure.text.TextComponent
+import org.bukkit.entity.Player
+import java.awt.Color
+import java.io.File
+import java.util.*
 
 class CopyCommand : Command("copy") {
     init {
         description("指定したGUIを自分の物として別の名前でコピーします。")
+
+        usage {
+            textArgument("from") {
+                Files.guis.forEach {
+                    suggest(it.name, server.getOfflinePlayer(UUID.fromString(it.author)).name)
+                }
+            }
+
+            textArgument("to")
+        }
+    }
+
+    private val workspaceDirectory = File("./plugins/CustomGUI/workspaces/")
+    override fun CommandContext.execute() {
+        if (sender !is Player) {
+            pluginMessageFail("プレイヤーのみがこのコマンドを実行できます。")
+            return
+        }
+
+        val fromGui = args[0].let { Files.findByName(it) }
+        val toGui = args[1].let { Files.findByName(it) }
+
+        if (fromGui == null) {
+            pluginMessage {
+                bold(args[0], Color.RED)
+                append("は存在しません。", Color.RED)
+            }
+            return
+        }
+
+        val normalizer = "[a-zA-Z0-9-_]+".toRegex()
+
+        if (!normalizer.matches(args[1])) {
+            pluginMessage {
+                bold(args[1], Color.RED)
+                append("は使用できない文字が含まれています。GUI名には半角英数字とハイフン、アンダースコアを使用することが出来ます。", Color.RED)
+            }
+            return
+        }
+
+        if (toGui != null) {
+            val author = server.getOfflinePlayer(UUID.fromString(toGui.author))
+            if (toGui.author == player!!.uniqueId.toString() || player!!.hasPermission("customgui.gui.delete")) {
+                pluginMessage {
+                    bold(args[1])
+                    append("という名前のGUIは")
+                    bold(author.name ?: "<UnknownPlayer>")
+                    appendln("が既に作成しています。上書きしますか？")
+                    append("チャットに入力")
+                    appendln(":", Color.LIGHT_GRAY)
+                    append("はい", Color.GREEN)
+                    append(":", Color.LIGHT_GRAY)
+                    boldln("Yes(Y)", Color.GREEN)
+                    append("いいえ", Color.RED)
+                    append(":", Color.LIGHT_GRAY)
+                    bold("No(N)", Color.RED)
+                }
+
+                CustomGUIListener.waitForChat(player!!) {
+                    it.isCancelled = true
+                    val message = (it.message() as TextComponent).content()
+                    when (message.lowercase()) {
+                        "yes", "y", "はい" -> {
+                            Files.save(
+                                GUI(
+                                    player!!.uniqueId.toString(),
+                                    player!!.name,
+                                    fromGui.data
+                                )
+                            )
+
+                            pluginMessage {
+                                bold(args[1], Color.GREEN)
+                                append("を")
+                                bold(fromGui.name, Color.GREEN)
+                                append("で上書きしました。", Color.GREEN)
+                            }
+                        }
+
+                        else -> {
+                            pluginMessage {
+                                bold(args[1], Color.RED)
+                                append("の上書きをキャンセルしました。", Color.RED)
+                            }
+                        }
+                    }
+                }
+            } else {
+                pluginMessage {
+                    bold(args[1], Color.RED)
+                    append("という名前のGUIは", Color.RED)
+                    bold(author.name ?: "<UnknownPlayer>", Color.RED)
+                    append("が既に作成しています。上書きするには、", Color.RED)
+                    bold("customgui.gui.delete", Color.RED)
+                    append("の権限を所有している必要があります。", Color.RED)
+                }
+            }
+            return
+        }
+
+        Files.save(GUI(player!!.uniqueId.toString(), args[1], fromGui.data))
+        pluginMessage {
+            bold(args[0], Color.GREEN)
+            append("を", Color.GREEN)
+            bold(args[1], Color.GREEN)
+            append("にコピーしました。", Color.GREEN)
+        }
     }
 }
