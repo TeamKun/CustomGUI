5fa3b8a490175dbedaa5522c4df764189de46a7c
Kotx <developer.hazuku@gmail.com>
2021-07-27 02:59:40 +0900

    implement ListCommand

--- plugin/src/main/kotlin/dev/kotx/customgui/commands/ListCommand.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/commands/ListCommand.kt
@@ -1,9 +1,37 @@
 package dev.kotx.customgui.commands
 
+import dev.kotx.customgui.Files
 import dev.kotx.flylib.command.Command
+import dev.kotx.flylib.command.CommandContext
+import java.awt.Color
+import java.util.*
 
 class ListCommand : Command("list") {
     init {
         description("全てのGUIの名前と作成者を表示します。")
+    }
+
+    override fun CommandContext.execute() {
+        if (Files.guis.isEmpty()) {
+            pluginMessageFail("一つもGUIが作成されていません。")
+        } else {
+            pluginMessage {
+                append("GUI一覧:", Color.GREEN)
+                append("(", Color.LIGHT_GRAY)
+                append(Files.guis.size.toString(), Color.GREEN)
+                appendln(")", Color.LIGHT_GRAY)
+
+                Files.guis.forEach {
+                    append(it.name, Color.RED)
+                    append("(", Color.LIGHT_GRAY)
+                    append(
+                        it.author.let { server.getOfflinePlayer(UUID.fromString(it)) }.name ?: "<UNKNOWN>",
+                        Color.RED
+                    )
+                    append(")", Color.LIGHT_GRAY)
+                    append(" ")
+                }
+            }
+        }
     }
 }
