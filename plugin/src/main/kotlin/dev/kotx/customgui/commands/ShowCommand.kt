6497b8117a021e7af7a6d9d4bd28ad2185e3c51f
kotlin <developer.hazuku@gmail.com>
2021-08-09 06:32:45 +0900

    a

--- plugin/src/main/kotlin/dev/kotx/customgui/commands/ShowCommand.kt
+++ plugin/src/main/kotlin/dev/kotx/customgui/commands/ShowCommand.kt
@@ -1,9 +1,12 @@
 package dev.kotx.customgui.commands
 
 import dev.kotx.flylib.command.Command
+import org.koin.core.component.KoinComponent
 
-class ShowCommand : Command("show") {
+class ShowCommand : Command("show"), KoinComponent {
     init {
         description("指定したGUIを指定したプレイヤーにクリック可能なGUI形式で表示します。自分以外に表示させるには権限が必要です。")
     }
+
+
 }
